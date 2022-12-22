e918412161e7309b3a55baf98194b8d09dd09bba
sergiogonzalez24 <sergiomanuel2020@gmail.com>
2022-12-22 11:14:02 -0600

    Agregar archivos a flutter

---
+++ sa_tools/lib/learn_flutter.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class LearnFlutterPage extends StatefulWidget {
+  const LearnFlutterPage({super.key});
+
+  @override
+  State<LearnFlutterPage> createState() => _LearnFlutterPageState();
+}
+
+class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Learn Flutter'),
+          automaticallyImplyLeading: false,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios_new),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ),
+        body: Column(
+          children: [
+            Image.asset('images/meme.jpg'),
+            const SizedBox(height: 10),
+            const Divider(
+              color: Colors.black,
+            ),
+            Container(
+              color: Colors.blueGrey,
+              child: const Text('This is a meme'),
+            )
+          ],
+        ));
+  }
+}
